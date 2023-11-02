eceb7b3b226ced13c560568fc314c0104d99203a
Jinmoyee Thakuria <110217586+Jinmoyee@users.noreply.github.com>
2023-11-02 19:55:49 +0530

    Connected to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: "../.env" })
 import express from 'express'
+import mongoose from 'mongoose'
 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to mongo")
+}).catch((err) => {
+    console.log(err)
+})
 const app = express()
 
 app.listen(3000, () => {
     console.log("Server is working")
 })
+
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log("Server up and running!")
+//         .catch((error) => console.log(error.message)
+// mongoose.set('useFindAndModify', false)
+// mongoose.connect(config.DB,{ useNewUrlParser: true });
